83bd3958d9da8bfa4118614632cc6fe390c89140
jason.chen <jason.chen@titansoft.com.sg>
2023-10-29 16:54:28 +0800

    try language

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,12 @@ import {defaultTheme, defineUserConfig} from "vuepress";
 export default defineUserConfig({
   base: '/blog/',
   locales: {
-    '/': {
+    '/blog/': {
       lang: 'en-US',
       title: 'Jasonnnnnnnn\'s Blog',
       description: 'blog for articles'
     },
-    '/zh/': {
+    '/blog/zh/': {
       lang: 'zh-CN',
       title: 'Jasonnnnnnnn\'s 部落格',
       description: '紀錄文章之地'
