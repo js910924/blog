d28cccc3a8024e0d5feae6763b921ce56bfb694a
jason.chen <jason.chen@titansoft.com.sg>
2023-10-29 16:08:22 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  base: '/blog/'
+  base: '/blog/',
+  title: 'Jasonnnnnnnn\'s Blog',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/blog' },
+      {
+        text: 'Other Links',
+        items: [
+          { text: 'Github', link: 'https://github.com/js910924' },
+          { text: 'Google', link: 'https://www.google.com' }
+        ]
+      }
+    ]
+  }
 }
