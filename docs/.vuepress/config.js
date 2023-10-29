54ebbbae2b4819750c37b2dfef2bd7a7f62f83c1
jason.chen <jason.chen@titansoft.com.sg>
2023-10-29 16:52:29 +0800

    change to config.ts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,22 @@
-module.exports = {
+import {defaultTheme, defineUserConfig} from "vuepress";
+
+export default defineUserConfig({
   base: '/blog/',
-  title: 'Jasonnnnnnnn\'s Blog',
-  themeConfig: {
-    nav: [
-      { text: 'Home', link: '/blog' },
-      {
-        text: 'Other Links',
-        items: [
-          { text: 'Github', link: 'https://github.com/js910924' },
-          { text: 'Google', link: 'https://www.google.com' }
-        ]
-      }
-    ]
-  }
-}
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Jasonnnnnnnn\'s Blog',
+      description: 'blog for articles'
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'Jasonnnnnnnn\'s 部落格',
+      description: '紀錄文章之地'
+    }
+  },
+  theme: defaultTheme({
+    logo: '/images/logo.png',
+    repo: 'vuepress/vuepress-next',
+    docsDir: 'docs',
+  })
+})
