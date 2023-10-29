59718d522730bf4813f67c45582ce47206a33f51
jason.chen <jason.chen@titansoft.com.sg>
2023-10-29 17:16:45 +0800

    update github repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,12 @@ import {defaultTheme, defineUserConfig} from "vuepress";
 export default defineUserConfig({
   base: '/blog/',
   locales: {
-    '/blog/': {
+    '/': {
       lang: 'en-US',
       title: 'Jasonnnnnnnn\'s Blog',
       description: 'blog for articles'
     },
-    '/blog/zh/': {
+    '/zh/': {
       lang: 'zh-CN',
       title: 'Jasonnnnnnnn\'s 部落格',
       description: '紀錄文章之地'
@@ -16,7 +16,7 @@ export default defineUserConfig({
   },
   theme: defaultTheme({
     logo: '/images/logo.png',
-    repo: 'vuepress/vuepress-next',
+    repo: 'js910924/blog',
     docsDir: 'docs',
   })
 })
